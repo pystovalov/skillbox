c2bdd679c148dd8cefdedbec3465a76b89f34e17
pystovalov <a.a.pystovalov@yandex.ru>
2025-06-21 22:08:32 +0300

    create templates

--- mysite/shopapp/views.py
+++ mysite/shopapp/views.py
@@ -1,3 +1,14 @@
+from django.http import HttpResponse, HttpRequest
+
 from django.shortcuts import render
 
 # Create your views here.
+
+
+def shop_index(request: HttpRequest):
+    products = [('laptop', 2000), ('desktop', 1999), ('place', 322)]
+    context = {
+        "title": "Index",
+        "products": products,
+    }
+    return render(request, "shopapp/shop-index.html", context)
