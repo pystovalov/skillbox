367e0aff5b3c9b597ff7d4b98bfd1765c62ab950
pystovalov <a.a.pystovalov@yandex.ru>
2025-06-24 23:16:37 +0300

    add field in model product

--- mysite/shopapp/models.py
+++ mysite/shopapp/models.py
@@ -6,3 +6,6 @@ from django.db import models
 class Product(models.Model):
     name = models.CharField(max_length=150)
     description = models.TextField(null=False, blank=True)
+    price = models.DecimalField(default=0, max_digits=8, decimal_places=2)
+    discoutnt = models.SmallIntegerField(default=0)
+    created_at = models.DateTimeField(auto_now_add=True)
